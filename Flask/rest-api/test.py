824d1f2b74b94b9f879e418bc48695a3c47aa149
Yorgo Petsas <yorgopecas@gmail.com>
2023-01-20 18:44:15 +0100

    Improve test file

--- Flask/rest-api/test.py
+++ Flask/rest-api/test.py
@@ -1,30 +1,24 @@
 import requests 
-# I got an error when I tried to run so I had to "pip3 install requests"
 
 # Set the base URL
 BASE = "http://127.0.0.1:5000/"
 
-# Load those in to the DB part 1
+# FILL DB / Create Items.
+# We use this snippet in order to upload this data into the DB part 1
 # data = [
 # 		{"likes": 10, "name" : "Tim", "views" : 100000}, 
 # 		{"likes": 20, "name" : "Jim", "views" : 200000}, 
 # 		{"likes": 30, "name" : "Bim", "views" : 300000}]
-
-# Load those in to the DB part 2
+#
 # for i in range(len(data)):
 # 	response = requests.put(BASE + "video/" + str(i), data[i])
 # 	print(response.json())
+# FILL DB END
 
-# input()
-# reposnse = requests.delete(BASE + 'video/0')
+# Delete Item
+# resposnse = requests.delete(BASE + 'video/0')
 # print(response)
 
-#input()
-
+# Update Item. In this case we only update one argument: "likes".
 response = requests.patch(BASE + "video/2", {"likes" : 101})
 print(response.json())
-
-# Send get request to the helloworld endpoint
-# We are passing arguments data  
-# response2 = requests.put(BASE + "video/2", {"likes": 10, "name" : "Jim", "views" : 100000})
-
