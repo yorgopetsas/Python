e31176c026fc6bd57480b784648f510e208a707c
Yorgo Petsas <yorgopecas@gmail.com>
2023-01-21 14:07:50 +0100

    Import and register the blueprints in the "__init__.py"

--- Flask/advanced-flask-website/website/__init__.py
+++ Flask/advanced-flask-website/website/__init__.py
@@ -2,13 +2,12 @@ from flask import Flask
 
 def create_app():
 	app = Flask(__name__)
-	# We are going to secure/incript the key for the session
+	# We are going to secure/encript the key for the session
 	app.config['SECRET_KEY'] = 'laksjfonaoushvoanoaeetg'
 	
-	#from .views import views
-	#from .auth import auth
+	from .views import views
+	from .auth import auth
 	
-	#app.register_blueprint(views, url_prefix='/')
-	
+ 	app.register_blueprint(views, url_prefix='/')
+ 	app.register_blueprint(auth, url_prefix='/')
 	return app
-	
