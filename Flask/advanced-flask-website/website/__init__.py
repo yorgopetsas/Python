babc99b7286a5a8efdee99263d3bff5c9cf8292d
Yorgo Petsas <yorgopecas@gmail.com>
2023-01-21 14:00:12 +0100

    Configure Secret Key

--- Flask/advanced-flask-website/website/__init__.py
+++ Flask/advanced-flask-website/website/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app():
 	app = Flask(__name__)
 	# We are going to secure/incript the key for the session
-	#app.config['SECRET_KEY'] = 'laksjfonaoushvoanoaeetg'
+	app.config['SECRET_KEY'] = 'laksjfonaoushvoanoaeetg'
 	
 	#from .views import views
 	#from .auth import auth
