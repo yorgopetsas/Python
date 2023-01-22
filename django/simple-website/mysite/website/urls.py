903ca675b8f01262601b9a052d41cc764feb2541
Yorgo Petsas <yorgopecas@gmail.com>
2023-01-22 20:20:57 +0100

    Django Basic

--- django/simple-website/mysite/website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-	path('about', views.about, name="about"),
-	path('project', views.project, name="project"),
-	path('django', views.django, name="django"),
-]
