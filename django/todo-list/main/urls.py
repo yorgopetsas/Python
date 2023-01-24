0a1f4920d232cc76036f2c7d49a23eae055d1c89
Yorgo Petsas <yorgopecas@gmail.com>
2023-01-24 04:33:39 +0100

    Main app URL's configured

--- django/todo-list/main/urls.py
+++ django/todo-list/main/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 
 # Import "views.py" file from current folder
+from . import views
+
+urlpatterns = [
+	path("<int:id>", views.index, name="index"),
+	path("", views.home, name="home"),
+	path("home/", views.home, name="home"),
+	path("create/", views.create, name="create"),
+	path("view/", views.view, name="view"),
+]
