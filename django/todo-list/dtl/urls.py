412cf94b62f314a034b9c8d0779eeafd97d01e9b
Yorgo Petsas <yorgopecas@gmail.com>
2023-01-24 04:30:51 +0100

    URLs setup

--- django/todo-list/dtl/urls.py
+++ django/todo-list/dtl/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path, include
+from register import views as v
+from django.contrib.auth import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path("register/", v.register, name="register"),
+    path('', include("main.urls")),
+    path('', include("django.contrib.auth.urls")),
 ]
 
