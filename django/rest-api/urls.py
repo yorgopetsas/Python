5c322a7fb4cdc005c4b1398b972dec8bf927aed0
Yorgo Petsas <yorgopecas@gmail.com>
2023-01-24 22:17:40 +0100

    REST API on DJANGO 4.1 stable beta

---
+++ django/rest-api/urls.py
@@ -0,0 +1,10 @@
+#from django.contrib import admin
+from django.urls import path
+from book_api.views import book_list, book_create, book
+
+urlpatterns = [
+    path('', book_create),
+    path('list/', book_list),
+    path('<int:pk>', book),
+    path('https://django-rest-api.amanitasolutions.com/', book_list),
+]
