09fd60aa52fd7024e5c3a0a49f91f18704c41b5d
Yorgo Petsas <yorgopecas@gmail.com>
2023-01-24 04:31:11 +0100

    Setup Apps and Global Variables

--- django/todo-list/dtl/settings.py
+++ django/todo-list/dtl/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    "crispy_forms", 
+    'main.apps.MainConfig',
+    'register.apps.RegisterConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+CRISPY_TEMPLATE_PACK="bootstrap4"
+
+LOGIN_REDIRECT_URL = "/view/"
+LOGOUT_REDIRECT_URL = "/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
